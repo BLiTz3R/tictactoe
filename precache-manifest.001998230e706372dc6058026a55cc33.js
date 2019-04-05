b664e0204c4014f03101e4f8f6f52dc7b831e647
George <bl1723r@gmail.com>
2019-04-06 01:13:33 +0300

    Updates

---
+++ precache-manifest.001998230e706372dc6058026a55cc33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  },
+  {
+    "revision": "53e7dd462ea82fbdc90f",
+    "url": "/tictactoe/static/js/main.2735260c.chunk.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tictactoe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "53e7dd462ea82fbdc90f",
+    "url": "/tictactoe/static/css/main.4859587e.chunk.css"
+  },
+  {
+    "revision": "ae0f6cc86d303e49e22d72560b79096a",
+    "url": "/tictactoe/index.html"
+  }
+];
